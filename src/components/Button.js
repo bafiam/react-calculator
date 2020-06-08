ba8eeb1a67a74e6fcd21ddcd54e694431af30162
bafiam <stevebafiam@gmail.com>
2020-06-08 13:18:43 +0300

    fix:add button type

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,8 +6,9 @@ class Button extends Component {
   render() {
     const { name, value, onClick } = this.props;
     return (
-      // eslint-disable-next-line react/button-has-type
+
       <button
+        type="button"
         className={name}
         value={value}
         onClick={onClick.bind(null, value)}
