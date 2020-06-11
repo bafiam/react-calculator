1c8850df4deb99b8cab343fd1ef76512c5b1c35f
bafiam <stevebafiam@gmail.com>
2020-06-11 19:22:31 +0300

    fix the use of more than one dot

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,41 +1,61 @@
-import Big from 'big.js';
+import Big from "big.js";
 
 // eslint-disable-next-line consistent-return
 const operate = (numberOne, numberTwo, operation) => {
   let a;
   let b;
+  if (numberOne.includes(".") || numberTwo.includes(".")) {
+  }
 
   if (numberOne) {
-    a = new Big(numberOne);
+    if (numberOne.includes(".") && numberOne.split(".").length - 1 === 1) {
+      a = new Big(numberOne);
+    } else if (
+      numberOne.includes(".") &&
+      numberOne.split(".").length - 1 !== 1
+    ) {
+      return "Math Error";
+    } else {
+      a = new Big(numberOne);
+    }
   }
 
   if (numberTwo) {
-    b = new Big(numberTwo);
+    if (numberTwo.includes(".") && numberTwo.split(".").length - 1 === 1) {
+      b = new Big(numberTwo);
+    } else if (
+      numberTwo.includes(".") &&
+      numberTwo.split(".").length - 1 !== 1
+    ) {
+      return "Math Error";
+    } else {
+      b = new Big(numberTwo);
+    }
   }
 
-  if (operation === '%') {
+  if (operation === "%") {
     if (numberOne) {
       return a.mul(0.01).valueOf();
     }
   }
-  if (operation === '-') {
+  if (operation === "-") {
     return a.minus(b).valueOf();
   }
 
-  if (operation === '+') {
+  if (operation === "+") {
     return a.plus(b).valueOf();
   }
 
-  if (operation === '*') {
+  if (operation === "*") {
     return a.mul(b).valueOf();
   }
 
-  if (operation === '÷') {
-    if (numberOne === '0' && numberTwo === '0') {
-      return 'Math Error';
+  if (operation === "÷") {
+    if (numberOne === "0" && numberTwo === "0") {
+      return "Math Error";
     }
-    if (numberOne !== '0' && numberTwo === '0') {
-      return 'Math Error';
+    if (numberOne !== "0" && numberTwo === "0") {
+      return "Math Error";
     }
 
     return a.div(b).valueOf();
